35a69465b94e725147a2b54c08e6e59ed2be4ac4
Lincoln Scheer <63661070+BikeLinc@users.noreply.github.com>
2022-03-22 08:39:41 -0600

    Vertex and Uniform Buffers
    Basic code for rendering a spinning multi-colored quad with vertex and indices buffers. Data is passed into the vertex shader via a Uniform Buffers
    
        master

--- Application.h
+++ Application.h
@@ -2,6 +2,7 @@
 
 #include "RoastHeader.h"
 
+// Represents the indices of a VkQueue Family
 struct QueueFamilyIndices {
     std::optional<uint32_t> graphicsFamily;
     std::optional<uint32_t> presentFamily;
@@ -11,18 +12,74 @@ struct QueueFamilyIndices {
     }
 };
 
+// Represents the SwapChain support details
 struct SwapChainSupportDetails {
     VkSurfaceCapabilitiesKHR capabilities;
     std::vector<VkSurfaceFormatKHR> formats;
     std::vector<VkPresentModeKHR> presentModes;
 };
 
+struct Vertex {
+    glm::vec2 pos;
+    glm::vec3 color;
+
+    static VkVertexInputBindingDescription getBindingDescription() {
+        VkVertexInputBindingDescription bindingDescription{};
+        bindingDescription.binding = 0;
+        bindingDescription.stride = sizeof(Vertex);
+        bindingDescription.inputRate = VK_VERTEX_INPUT_RATE_VERTEX;
+
+        return bindingDescription;
+    }
+
+    static std::array<VkVertexInputAttributeDescription, 2> getAttributeDescriptions() {
+        std::array<VkVertexInputAttributeDescription, 2> attributeDescriptions{};
+
+        attributeDescriptions[0].binding = 0;
+        attributeDescriptions[0].location = 0;
+        attributeDescriptions[0].format = VK_FORMAT_R32G32_SFLOAT;
+        attributeDescriptions[0].offset = offsetof(Vertex, pos);
+
+        attributeDescriptions[1].binding = 0;
+        attributeDescriptions[1].location = 1;
+        attributeDescriptions[1].format = VK_FORMAT_R32G32B32_SFLOAT;
+        attributeDescriptions[1].offset = offsetof(Vertex, color);
+
+        return attributeDescriptions;
+    }
+};
+
+struct UniformBufferObject {
+    glm::mat4 model;
+    glm::mat4 view;
+    glm::mat4 proj;
+};
+
+const std::vector<Vertex> vertices = {
+    {{-0.5f, -0.5f}, {1.0f, 0.0f, 0.0f}},
+    {{0.5f, -0.5f}, {0.0f, 1.0f, 0.0f}},
+    {{0.5f, 0.5f}, {0.0f, 0.0f, 1.0f}},
+    {{-0.5f, 0.5f}, {1.0f, 1.0f, 1.0f}}
+};
+
+const std::vector<uint16_t> indices = {
+    0, 1, 2, 2, 3, 0
+};
+
+// Vulkan Renderer
 class Application {
 public:
     void run();
 
 private:
+    // GLFW Members
     GLFWwindow* window;
+    void initWindow();
+    static void framebufferResizeCallback(GLFWwindow* window, int width, int height);
+    int nbFrames = 0;
+    double lastTime = 0.0;
+
+    // Vulkan Members
     VkInstance instance;
     VkDebugUtilsMessengerEXT debugMessenger;
     VkPhysicalDevice physicalDevice;
@@ -35,6 +92,7 @@ private:
     std::vector<VkImageView> swapChainImageViews;
     VkFormat swapChainImageFormat;
     VkExtent2D swapChainExtent;
+    VkDescriptorSetLayout descriptorSetLayout;
     VkPipelineLayout pipelineLayout;
     VkRenderPass renderPass;
     VkPipeline graphicsPipeline;
@@ -47,7 +105,17 @@ private:
     bool framebufferResized = false;
     uint32_t currentFrame = 0;
 
-    void initWindow();
+    VkBuffer vertexBuffer;
+    VkDeviceMemory vertexBufferMemory;
+    VkBuffer indexBuffer;
+    VkDeviceMemory indexBufferMemory;
+
+    std::vector<VkBuffer> uniformBuffers;
+    std::vector<VkDeviceMemory> uniformBuffersMemory;
+
+    VkDescriptorPool descriptorPool;
+    std::vector<VkDescriptorSet> descriptorSets;
+    
     void initVulkan();
     void loop();
     void renderFrame();
@@ -81,5 +149,16 @@ private:
     void createSyncObjects();
     void reCreateSwapchain();
     void cleanUpSwapchain();
-    static void framebufferResizeCallback(GLFWwindow* window, int width, int height);
+    void createVertexBuffer();
+    uint32_t findMemoryType(uint32_t typeFilter, VkMemoryPropertyFlags properties);
+    void createBuffer(VkDeviceSize size, VkBufferUsageFlags usage, VkMemoryPropertyFlags properties, VkBuffer& buffer, VkDeviceMemory& bufferMemory);
+    void copyBuffer(VkBuffer srcBuffer, VkBuffer dstBuffer, VkDeviceSize size);
+    void createIndexBuffer();
+    void createDescriptorSetLayout();
+    void createUniformBuffers();
+    void updateUniformBuffer(uint32_t currentImage);
+    void createDescriptorPool();
+    void createDescriptorSets();
+    void setWindowFPS(GLFWwindow* win);
+
 };
--- RoastHeader.h
+++ RoastHeader.h
@@ -1,8 +1,12 @@
 #pragma once
 
+// Vulkan SDK via GLFW
 #define GLFW_INCLUDE_VULKAN
+
+// GLFW Library
 #include <GLFW/glfw3.h>
 
+// Standard Template Library
 #include <iostream>
 #include <stdexcept>
 #include <vector>
@@ -14,19 +18,34 @@
 #include <limits>
 #include <algorithm>
 #include <fstream>
+#include <array>
+#include <chrono>
 
-const uint32_t WIDTH = 800;
-const uint32_t HEIGHT = 600;
-const int MAX_FRAMES_IN_FLIGHT = 2;
+
+
+
+
+// OpenGL Mathmatics Libary
+#define GLM_FORCE_RADIANS
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
 
 // Validation layers to enable.
 const std::vector<const char*> validationLayers = { "VK_LAYER_KHRONOS_validation" };
 
-// Required swapchain extensions
+// Required swapchain extensions.
 const std::vector<const char*> deviceExtensions = { VK_KHR_SWAPCHAIN_EXTENSION_NAME };
 
+// Enable validation layers if not in release configuration.
 #ifdef NDEBUG
 const bool enableValidationLayers = false;
 #else
 const bool enableValidationLayers = true;
 #endif
+
+
+// CONSTANTS
+const uint32_t WIDTH = 800;
+const uint32_t HEIGHT = 600;
+const int MAX_FRAMES_IN_FLIGHT = 2;
+
